b6cdbb8491cdc70b9005931a7e7f235690485c87
Jose Luis Sanchez <joseluis.sanchez84@gmail.com>
2018-01-01 12:51:20 -0500

    I created the PracticeYourEnglishActivity activity.

---
+++ app/src/main/java/com/example/android/yourenglishvocabulary/ui/PracticeYourEnglishActivity.java
@@ -0,0 +1,30 @@
+package com.example.android.yourenglishvocabulary.ui;
+
+import android.net.Uri;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.android.yourenglishvocabulary.R;
+
+public class PracticeYourEnglishActivity extends AppCompatActivity implements PracticeYourEnglishFragment.OnFragmentInteractionListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_practice_your_english);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        PracticeYourEnglishFragment fragment = new PracticeYourEnglishFragment();
+        fragmentTransaction.add(R.id.practice_fragment, fragment);
+        fragmentTransaction.commit();
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
+    }
+}
