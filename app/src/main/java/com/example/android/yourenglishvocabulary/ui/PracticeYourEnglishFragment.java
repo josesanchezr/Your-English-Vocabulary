421ace953f52e3bc5eb9e7a2eca42f1670f1800c
Jose Luis Sanchez <joseluis.sanchez84@gmail.com>
2018-01-01 12:58:34 -0500

    I created the PracticeYourEnglishFragment fragment.

---
+++ app/src/main/java/com/example/android/yourenglishvocabulary/ui/PracticeYourEnglishFragment.java
@@ -0,0 +1,67 @@
+package com.example.android.yourenglishvocabulary.ui;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.example.android.yourenglishvocabulary.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * to handle interaction events.
+ * create an instance of this fragment.
+ */
+public class PracticeYourEnglishFragment extends Fragment {
+
+    @BindView(R.id.word_vs_words_button)
+    Button wordVsWordsButton;
+
+    @BindView(R.id.word_vs_images_button)
+    Button wordVsImagesButton;
+
+    @BindView(R.id.sound_vs_words_button)
+    Button soundVsWords;
+
+    @BindView(R.id.sound_vs_images_button)
+    Button soundVsImages;
+
+    @BindView(R.id.image_vs_words_button)
+    Button imageVsWords;
+
+    public PracticeYourEnglishFragment() {
+
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View rootView = inflater.inflate(R.layout.fragment_practice_your_english, container, false);
+        ButterKnife.bind(this, rootView);
+
+        wordVsWordsButton.setOnClickListener((view) -> {
+            Intent intent = new Intent(view.getContext(), WordVsWordsActivity.class);
+            startActivity(intent);
+        });
+
+        return rootView;
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+}
